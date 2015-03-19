a6d989d6f79b5372a00511744c8ab0ccab44f22c
Connor Hicks <connor@cohix.ca>
2015-03-18 20:00:21 -0400

    Changed a word

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 		<div id = "footerContainer"> 
 
-			<h3 id = "cohix"> Website managed by <a href = "http://cohix.ca"> Connor Hicks </a> </h3> 
+			<h3 id = "connor"> Website managed by <a href = "http://cohix.ca"> Connor Hicks </a> </h3> 
 
 		</div>
 
